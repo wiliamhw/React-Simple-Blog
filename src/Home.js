9ecb179df1e6f655ea0755c3eba42073f1b44156
William <wiliamwijaya1985@gmail.com>
2021-08-16 15:07:37 +0700

    show blogs by author

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,18 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  const [name, setName] = useState('mario');
-
-  useEffect(() => {
-    console.log('use effect ran');
-    console.log(name);
-  }, [name]);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="Home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-      <BlogList 
-        blogs={blogs.filter((blog) => blog.author === 'mario')} 
-        title="Mario's Blogs!"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName('luigi')}>Change Name</button>
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {blogs && <BlogList 
+          blogs={blogs.filter(blog => blog.author === 'mario')} 
+          title="Mario's Blogs!"
+      />}
     </div>
   );
 }
