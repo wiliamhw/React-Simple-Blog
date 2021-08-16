ee02c7e059c65af48203a4dfc65cf647a268052a
William <wiliamwijaya1985@gmail.com>
2021-08-16 17:13:33 +0700

    add new blog

--- src/Create.js
+++ src/Create.js
@@ -1,23 +1,54 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then (() => {
+      console.log('New blog added');
+      setIsPending(false);
+    })
+  }
+
   return ( 
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input 
           type="text"
           required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog title:</label>
         <textarea 
           required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        { !isPending && <button>Add Blog</button> }
+        { isPending && <button disabled>Adding blog...</button> }
       </form>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,9 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+  const { data: blogs, isPending, error } = (
+    useFetch('http://localhost:8000/blogs')
+  );
 
   return (
     <div className="Home">
